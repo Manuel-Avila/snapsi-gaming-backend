220d3592d79008a9bcbd3110542e872114f4e0c0
Manuel-Avila <mavila_22@alu.uabcs.mx>
2026-04-26 13:28:15 -0700

    fix(db)

--- database/migrate.js
+++ database/migrate.js
@@ -26,6 +26,7 @@ const statements = [
         caption TEXT,
         tags JSON,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
         FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
     );`,
   `CREATE TABLE IF NOT EXISTS stories (
